fb059e6bdf4efb688d2f382f1db802c4c14e3baa
Baba <Baba@BABA-PC>
2016-09-06 23:31:11 +0200

    User Repo v1

---
+++ Repository/Repos/IUserRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Repository.Repos
+{
+    interface IUserRepo
+    {
+        void AddReading(Readings reading);
+        IQueryable<Readings> GetReadings();
+        IQueryable<Invoices> GetInvoices();
+        Appartments GetAppartmentData();
+        void ModifyAppartment(string owner, double size, int residents);
+    }
+}
---
+++ Repository/Repos/UserRepo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using System.IO;
+using System.Data.Entity;
+
+namespace Repository.Repos
+{
+    class UserRepo : IUserRepo
+    {
+
+        int APPARTMENTID;
+
+        static string loc = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Entities\\TarsashazDB.mdf");
+        static string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;
+AttachDbFilename=" + loc + ";Integrated Security=True";
+        static DbContext context = new DbContext(connString);
+        ReadingEFRepo readingRepo;
+        AppartmentEFRepo appartmentRepo;
+        InvoiceEFRepo invoiceRepo;
+        public void InitRepos()
+        {
+            readingRepo = new ReadingEFRepo(context);
+            appartmentRepo = new AppartmentEFRepo(context);
+            invoiceRepo = new InvoiceEFRepo(context);
+        }
+        public void AddReading(Readings reading)
+        {
+            readingRepo.Insert(reading);
+        }
+
+        public Appartments GetAppartmentData()
+        {
+            return appartmentRepo.GetById(APPARTMENTID);
+        }
+
+        public IQueryable<Invoices> GetInvoices()
+        {
+            return invoiceRepo.GetInvoicesByAppartment(APPARTMENTID);
+        }
+
+        public IQueryable<Readings> GetReadings()
+        {
+            return readingRepo.GetReadingsByAppartment(APPARTMENTID);
+        }
+
+        public void ModifyAppartment(string owner, double size, int residents)
+        {
+            appartmentRepo.Modify(APPARTMENTID, owner, residents, size);
+        }
+    }
+}
