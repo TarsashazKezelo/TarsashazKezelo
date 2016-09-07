ae2bb190e266ccf13c7429b7e96d43365803d13b
tothgergo <greg.toth93@gmail.com>
2016-09-07 02:42:04 +0200

    interfészek egybevonása

---
+++ tarsashazkezelo_admin_frontend/tarsashazkezelo_admin_frontend/Interfaces/IAdminFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tarsashazkezelo_admin_frontend.Model;
+
+namespace tarsashazkezelo_admin_frontend.Interfaces
+{
+    interface IAdminFunctions
+    {
+        ObservableCollection<Apartment> GetApartments();
+
+        void ModifyApartment(Apartment a);
+
+        void AddBuildingInvoice(BuildingInvoice bi);
+
+        ObservableCollection<BuildingInvoice> GetBuildingInvoices();
+
+        ObservableCollection<Invoice> GetInvoices();
+
+        void AddMainMeter(MainMeter mm);
+
+        ObservableCollection<MainMeter> GetMainMeters();
+
+        void AddMeter(Meter m);
+
+        ObservableCollection<Meter> GetMeters();
+
+        void AddService(Service s);
+
+        ObservableCollection<Service> GetServices();
+    }
+}
