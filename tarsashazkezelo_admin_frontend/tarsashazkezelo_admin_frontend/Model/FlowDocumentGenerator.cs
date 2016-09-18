525439be1de74217f3286caf8cc68536f25b8d76
tothgergo <greg.toth93@gmail.com>
2016-09-19 01:56:40 +0200

    admin felület design

---
+++ tarsashazkezelo_admin_frontend/tarsashazkezelo_admin_frontend/Model/FlowDocumentGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarsashazkezelo_admin_frontend.Model
+{
+    class FlowDocumentGenerator
+    {
+    }
+}
